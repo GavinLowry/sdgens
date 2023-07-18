263563cdd2dec417fcc6c111f8adcf6b5085277d
Gavin Lowry <gavinlowry@gmail.com>
2023-07-18 14:58:40 -0600

    npc portrait

--- README.md
+++ README.md
@@ -3,12 +3,14 @@ Dexie reference
 https://dexie.org/docs/API-Reference#add-items
 
 
+
 TODO:
-support adding images to npc objects
+click npc image to make it fullscreen to show to players
 flag "orphan" items whose project has been removed
-support change item's project and copy item to other project
+support change item's project
 
 DONE:
+support npc portraits
 show npc form values in editable text inputs
 click npc from list to show it in a form. support delete
 fix the screen width problem
@@ -17,31 +19,6 @@ allow a toggle for this feature
 
 
 
-```bash
+
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/pages/shadowdark/npcs/page.tsx
+++ src/app/pages/shadowdark/npcs/page.tsx
@@ -9,66 +9,12 @@ import {generateName} from '../../../utils/random';
 
 import './npcs.css';
 
-interface Npc {
-	name?: string;
-	race?: string;
-	age?: string;
-	alignment?: string;
-	wealth?: string;
-	appearance?: string;
-	behavior?: string;
-	secret?: string;
-	occupation? :string;
-    [key: string]: string | undefined;
-}
-
-const attributes: {[key: string]: string[]} = {
-	name: [],
-	race: ['human','elf','dwarf','halfling','half-orc','goblin'],
-	occupation: ['gravedigger','tax collector','baker','locksmith','carpenter','farmer','cook','cobbler','scholar','bartender','sailor','friar/nun','blacksmith','beggar','butcher','merchant'],
-	age: ['adolescent','young adult','adult','middle-aged','elderly','ancient'],
-	alignment: ['lawful','neutral','chaotic'],
-	wealth: ['poor','standard','successful','wealthy','extravagant'],
-	appearance: ['balding','stocky build','very tall','beauty mark','one eye','braided hair','muscular','white hair','facial scar','willowy build','sweaty','cleft chin','frail','big eyebrows','tattooed','floppy hat','gold tooth','six fingers','very short','large nose'],
-	behavior: ['spits','always eating','moves quickly','card tricks','prays aloud','writes in diary','apologetic','slaps backs','drops things','swears oath','makes puns','rare accent','easily spooked','forgetful','speaks quietly','twitches','moves slowly','speaks loudly','swaggers','smokes pipe'],
-	secret: ['hiding a fugitive','adores baby animals','obsessed with fire','in a religious cult','is a half-demon','was a wizard\'s apprentice','needlessly picks pockets','has a false identity','afraid of storms','has functional gills','in deep gambling debt','works as a smuggler','is a werewolf','can actually smell lies','cast out of wealthy family','in love with a bartender','left the thieves\' guild','best friends with a prince','retired crawler','has a pet basilisk'],
-};
-
-function roll(lo: number, hi: number): number {
-	const span = hi - lo + 1;
-	return Math.floor(Math.random() * span) + lo;
-}
-
-function chooseRandom(attribute: string): string {
-	const options = attributes[attribute];
-	return options[roll(0,options.length-1)];
-}
-
-function generate(): Npc {
-	const npc: Npc = {};
-	const attributeNames = Object.keys(attributes);
-	attributeNames.forEach(name => {
-		if(name==='name') {
-			npc.name = generateName();
-		} else {
-			npc[name] = chooseRandom(name);
-		}
-	})
-	return npc;
-}
-
-function reroll(attribute: string) {
-	const newValue = attribute === 'name'
-	? generateName()
-	: chooseRandom(attribute);
-	return newValue;
-}
-
 const Npcs: FC = () => {
 	const {selectedProject, setSelectedProject} = useContext(SelectedProject);
 	const {filterByProject, setFilterByProject} = useContext(FilterByProject);
 	const [newNpc, setNewNpc] = useState<Npc | undefined>();
 	const [npcList, setNpcList] = useState<Npc[]>([]);
+	const [bigImage, setBigImage] = useState<File | undefined>();
 
 	useEffect(() => {
 		updateNpcList();
@@ -137,7 +83,7 @@ const Npcs: FC = () => {
 		try {
 			if (newNpc.id) {
 				npcsTable
-				.delete(newNpc.id)
+				.delete(newNpc.id as string)
 				.then(() => {
 					updateNpcList();
 					setNewNpc(undefined);
@@ -155,6 +101,14 @@ const Npcs: FC = () => {
 		}));
 	}
 
+	function onShowPortrait(): void {
+		setBigImage(newNpc!.portrait);
+	}
+
+	function onHidePortrait():void {
+		setBigImage(undefined);
+	}
+
 	function renderNpcForm(npc: Npc) {
 		function onChangeField(event: SyntheticEvent<HTMLInputElement>) {
 			const target = event.target as HTMLInputElement;
@@ -167,10 +121,11 @@ const Npcs: FC = () => {
 		}
 
 		function renderLine(label: string, value: string | undefined) {
+			if (label==='portrait'){ console.log({value})}
 			return (
 				<div className="npc-line" key={label}>
 					<div>{label}:</div>
-					<input type="text" name={label} value={value} onChange={onChangeField} />
+					<input type="text" name={label} value={value as string} onChange={onChangeField} />
 					<button onClick={() => onReroll(label)}>reroll</button>
 				</div>
 			);
@@ -179,12 +134,34 @@ const Npcs: FC = () => {
 		const attributeNames = Object.keys(attributes);
 		return (
 			<div className="npc-form">
-				{attributeNames.map(n => renderLine(n, npc[n]))}
+				{attributeNames.map(n => {
+					return renderLine(n, npc[n] as string)
+				})}
 				<div className="ai-prompt">
 					<div>AI prompt</div>
 					<div className="ai-prompt-content">
 					{npc.wealth} {npc.age} {npc.race} {npc.occupation}. {npc.appearance}, {npc.behavior}, {npc.secret}.
 					</div>
+				</div>
+				{ npc.portrait &&
+					<div className="npc-portrait" onClick={onShowPortrait}>
+						<img src={URL.createObjectURL(npc.portrait as Blob)} />
+					</div>
+				}
+				<div>
+					<input
+						type="file"
+						name="myImage"
+						onChange={(event) => {
+							const target = event.target;
+							const files = target.files;
+							const portrait = files && files.length > 0 ? files[0] : undefined;
+							setNewNpc(pc => ({
+								...pc,
+								portrait,
+							}))
+						}}
+					/>
 				</div>
 				<div className="npc-form-button-row">
 					{
@@ -229,22 +206,80 @@ const Npcs: FC = () => {
 	}
 
 	return (
+		bigImage
+		?
+		<div onClick={onHidePortrait}>
+			<img src={URL.createObjectURL(bigImage as Blob)} />
+		</div>
+		:
 		<div>
-			<div>npcs</div>
-			<div>
-				{filterByProject ? '' : 'NOT'} filtering by project {selectedProject}
-			</div>
-			<button onClick={onGenerate}>Generate</button>
+			<button onClick={onGenerate}>Generate new NPC</button>
 			{newNpc && renderNpcForm(newNpc)}
 			<div className="npc-list">
 				{
 					npcList
-						.filter(npc => !filterByProject || parseInt(npc.projectId ?? '') === selectedProject)
+						.filter(npc => !filterByProject || parseInt(npc.projectId as string ?? '') === selectedProject)
 						.map(npc => renderStoredNpcListItem(npc))
 				}
 			</div>
 		</div>
 	);
+}
+
+interface Npc {
+	name?: string;
+	race?: string;
+	age?: string;
+	alignment?: string;
+	wealth?: string;
+	appearance?: string;
+	behavior?: string;
+	secret?: string;
+	occupation? :string;
+	portrait?: File;
+    [key: string]: string | File | undefined;
+}
+
+const attributes: {[key: string]: string[]} = {
+	name: [],
+	race: ['human','elf','dwarf','halfling','half-orc','goblin'],
+	occupation: ['gravedigger','tax collector','baker','locksmith','carpenter','farmer','cook','cobbler','scholar','bartender','sailor','friar/nun','blacksmith','beggar','butcher','merchant'],
+	age: ['adolescent','young adult','adult','middle-aged','elderly','ancient'],
+	alignment: ['lawful','neutral','chaotic'],
+	wealth: ['poor','standard','successful','wealthy','extravagant'],
+	appearance: ['balding','stocky build','very tall','beauty mark','one eye','braided hair','muscular','white hair','facial scar','willowy build','sweaty','cleft chin','frail','big eyebrows','tattooed','floppy hat','gold tooth','six fingers','very short','large nose'],
+	behavior: ['spits','always eating','moves quickly','card tricks','prays aloud','writes in diary','apologetic','slaps backs','drops things','swears oath','makes puns','rare accent','easily spooked','forgetful','speaks quietly','twitches','moves slowly','speaks loudly','swaggers','smokes pipe'],
+	secret: ['hiding a fugitive','adores baby animals','obsessed with fire','in a religious cult','is a half-demon','was a wizard\'s apprentice','needlessly picks pockets','has a false identity','afraid of storms','has functional gills','in deep gambling debt','works as a smuggler','is a werewolf','can actually smell lies','cast out of wealthy family','in love with a bartender','left the thieves\' guild','best friends with a prince','retired crawler','has a pet basilisk'],
+};
+
+function roll(lo: number, hi: number): number {
+	const span = hi - lo + 1;
+	return Math.floor(Math.random() * span) + lo;
+}
+
+function chooseRandom(attribute: string): string {
+	const options = attributes[attribute];
+	return options[roll(0,options.length-1)];
+}
+
+function generate(): Npc {
+	const npc: Npc = {};
+	const attributeNames = Object.keys(attributes);
+	attributeNames.forEach(name => {
+		if(name==='name') {
+			npc.name = generateName();
+		} else {
+			npc[name] = chooseRandom(name);
+		}
+	})
+	return npc;
+}
+
+function reroll(attribute: string) {
+	const newValue = attribute === 'name'
+	? generateName()
+	: chooseRandom(attribute);
+	return newValue;
 }
 
 export default Npcs;
