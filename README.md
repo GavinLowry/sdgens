e192e46c6a26be014f315c7f04b4abee2ceab89c
Gavin Lowry <gavinlowry@gmail.com>
2023-07-18 21:57:21 -0600

    map view

--- README.md
+++ README.md
@@ -5,17 +5,25 @@ https://dexie.org/docs/API-Reference#add-items
 
 
 TODO:
-click npc image to make it fullscreen to show to players
+only generate maps when you click a generate button
+name maps
+save maps
+mapview: leave "to" room selected after connect
+mapview: disable connect button unless a node is selected
+mapview: (maybe) fire a connect event on keyboard "c"
+roll room types and show on map
+land hex map page (maybe rename previous map page to cave map or dark map)
 flag "orphan" items whose project has been removed
 support change item's project
 
 DONE:
+in mapview support user-created connections
+click npc image to make it fullscreen to show to players
 support npc portraits
 show npc form values in editable text inputs
 click npc from list to show it in a form. support delete
 fix the screen width problem
-filter displayed lists to show only items that pertain to the current project
-allow a toggle for this feature
+toggle to filter displayed lists to show only npcs that pertain to the current project
 
 
 
--- src/app/pages/shadowdark/maps/page.tsx
+++ src/app/pages/shadowdark/maps/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import {useEffect, useState} from 'react'
-import MapView, {MapData, Point} from '../../../utils/mapview'
+import {useState} from 'react'
+import MapView, {MapData, RoomData} from '../../../utils/mapview'
 import {hexAreaPoints} from '../../../utils/hex'
 import {roll} from '../../../utils/random'
 
@@ -12,14 +12,9 @@ Shadowdark p. 130
 export default function Maps() {
     const [mapData, setMapData] = useState<MapData>();
 
-    useEffect(() => {
-        console.log('Maps()')
-        const md: MapData = generateMap(10);
-        setMapData(md);
-    }, [])
-
     function generateMap(roomCount: number): MapData {
-        const md: MapData = {rooms: []};
+        let lastId = 0;
+        const md: MapData = {rooms: [], info:{title:""}};
         const points = hexAreaPoints(3);
         const roomPoints = [];
         for (let i=0; i<roomCount; ++i) {
@@ -31,21 +26,42 @@ export default function Maps() {
         const heightMult = .5;
         const spacing = 12;
         md.rooms = roomPoints.map(p => ({
+            id: lastId++,
             title: "",
             location: {
                 x: p.x * spacing,
                 y: p.y * spacing * heightMult
             },
-            exits: []
         }));
 
         return md;
     }
 
+    function onConnect(from: RoomData, to: RoomData) {
+        console.log('onConnect',{from,to})
+        const fromRoom = mapData!.rooms.find(r => r.id === from.id);
+        console.log({mapData,fromRoom})
+        if (fromRoom && to) {
+            if (!fromRoom.hasOwnProperty('exits')) {
+                fromRoom.exits = [];
+            }
+            fromRoom.exits!.push({destination: to.id})
+        }
+    }
+
+    function onGenerate() {
+        // TODO: allow map size or room count selection
+        const md: MapData = generateMap(10);
+        setMapData(md);
+    }
+
     return (
         <div>
             <div>maps</div>
-            <MapView mapData={mapData} />
+            <button onClick={onGenerate}>generate</button>
+            {mapData &&
+                <MapView mapData={mapData} onConnect={onConnect} />
+            }
         </div>
     );
 }
